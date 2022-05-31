e132ebe8eaa94612e97a79d86e14ef6d61f16880
Elizabeth <Hindash@email.arizona.edu>
2022-05-30 18:08:11 -0700

    saving progress midway through app.js

--- app.js
+++ app.js
@@ -1,18 +1,83 @@
 const inquirer = require("inquirer");
 // const fs = require("fs");
 // const generatePage = require("./src/page-template.js");
-
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       // pass questions here
       type: "input",
       name: "name",
       message: "What is your name?",
     },
-  ])
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
 
-  .then((answers) => console.log(answers));
+const promptProject = (portfolioData) => {
+  portfolioData.projects = [];
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
+  ]);
+};
+promptUser()
+  .then((answers) => console.log(answers))
+  .then(promptProject)
+  .then((projectAnswers) => console.log(projectAnswers));
+
 // {
 //     // you will use the user input from here
 //   });
